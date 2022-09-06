51441b8640687dfdd04a9b33f29767142e866a27
edgcastillo <edgcastillo@gmail.com>
2022-09-06 08:29:24 -0400

    install psycopg2 adapter

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, status, HTTPException, Response
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -11,6 +13,15 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+try:
+    conn = psycopg2.connect(
+        host='localhost', database='fastapi', user='postgres', password='')
+    cursor = conn.cursor()
+except Exception as error:
+    print("Connecting to database failed")
+    print("Error: ", error)
 
 
 all_posts = [
@@ -25,6 +36,12 @@ def find_single_post(id):
             return post
 
 
+def find_post_index(id):
+    for index, post in all_posts:
+        if id == post["id"]:
+            return index, post
+
+
 @app.get("/")
 def root():
     return {"message": "Work in Progress"}
@@ -35,7 +52,7 @@ def get_posts():
     return {"data": all_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     new_post = post.dict()
     new_post["id"] = randrange(0, 1000000)
@@ -50,3 +67,12 @@ def find_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
     return {"data": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index, post = find_post_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    all_posts.pop(index)
+    Response(status_code=status.HTTP_204_NO_CONTENT)

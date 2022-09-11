85b2fcb9863636e429259dd5643dd118569f08df
edgcastillo <edgcastillo@gmail.com>
2022-09-11 10:09:30 -0400

    latest changes, adding users table and routes

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
-from pyexpat import model
+from enum import auto
 from sqlite3 import dbapi2
 from fastapi import Depends, FastAPI, status, HTTPException, Response
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from passlib.context import CryptContext
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas, utils
 from .database import engine, get_db
-from .schemas import Post
+
+# telling bcrypt what is the hashing algorithm
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+models.Base.metadata.create_all(bind=engine)  # create the tables
 
 app = FastAPI()
-
 
 while True:
     try:
         conn = psycopg2.connect(
-            host='localhost', database='fastapi', user='postgres', password='mzt6KYz2J6CXWqQH6hzR', cursor_factory=RealDictCursor)
+            host='localhost', database='fastapi', user='postgres', password='', cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         break
     except Exception as error:
@@ -32,16 +36,16 @@ def root():
     return {"message": "Work in Progress"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts """)
     # all_posts = cursor.fetchall()
     all_posts = db.query(models.Post).all()
-    return {"data": all_posts}
+    return all_posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s %s %s) RETURNING * """,
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -51,10 +55,10 @@ def create_post(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)  # retreive this newly created post from our database
-    return {"data": new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def find_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute(
     #     """ SELECT title, content, published, created_at FROM posts WHERE id = %s """, (str(id)))
@@ -63,7 +67,7 @@ def find_post(id: int, db: Session = Depends(get_db)):
     if not found_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
-    return {"data": found_post}
+    return found_post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -83,8 +87,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
     #                (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
@@ -97,4 +101,33 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
 
-    return {'data': post_query.first()}
+    return post_query.first()
+
+
+@app.get("/users", response_model=List[schemas.User])
+def get_users(db: Session = Depends(get_db)):
+    all_users = db.query(models.User).all()
+    return all_users
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.User)
+def get_user(id: int, db: Session = Depends(get_db)):
+    found_user = db.query(models.User).filter(models.User.id == id).first()
+    if not found_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with {id} does not exist")
+    return found_user
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 
-class Post(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
+
+
+class PostCreate(PostBase):
+    pass  # same as PostBase nothing new being passed
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
